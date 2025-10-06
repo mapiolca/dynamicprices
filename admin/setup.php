a762a8044406b81a3504f7c77b3f40db750fe131
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-10-06 16:13:17 +0200

    Fix saving of fixed fees coefficient

--- admin/setup.php
+++ admin/setup.php
@@ -108,7 +108,23 @@ if (!$user->admin) {
 }
 
 
-$action = 'edit';
+if ($action === 'set_LMDB_COST_PRICE_FIXED_FEES_COEF') {
+        $coefInput = GETPOST('LMDB_COST_PRICE_FIXED_FEES_COEF', 'alphanohtml');
+        $coefValue = ($coefInput === '') ? '' : price2num($coefInput, 'MU');
+
+        $result = dolibarr_set_const($db, 'LMDB_COST_PRICE_FIXED_FEES_COEF', $coefValue, 'chaine', 0, '', $conf->entity);
+        if ($result <= 0) {
+                setEventMessages($db->lasterror(), null, 'errors');
+        } else {
+                setEventMessages($langs->trans('SetupSaved'), null, 'mesgs');
+        }
+
+        $action = '';
+}
+
+if (empty($action)) {
+        $action = 'edit';
+}
 
 
 /*
