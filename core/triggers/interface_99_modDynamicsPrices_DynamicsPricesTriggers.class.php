d7a494a7aae02f4b6296426c5160c787df2902fb
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-11-27 14:17:17 +0100

    Add debug output for cost price global string
    Added debugging output for LMDB_COST_PRICE_ONLY global string.

--- core/triggers/interface_99_modDynamicsPrices_DynamicsPricesTriggers.class.php
+++ core/triggers/interface_99_modDynamicsPrices_DynamicsPricesTriggers.class.php
@@ -74,6 +74,7 @@ class InterfaceDynamicsPricesTriggers extends DolibarrTriggers
         global $db;
 
         if (!getDolGlobalString('LMDB_COST_PRICE_ONLY')) {
+			var_dump(getDolGlobalString('LMDB_COST_PRICE_ONLY'));
             if ($action === 'SUPPLIER_PRODUCT_BUYPRICE_CREATE' || $action === 'SUPPLIER_PRODUCT_BUYPRICE_MODIFY' || $action == 'SUPPLIER_PRODUCT_BUYPRICE_DELETE') {
                 require_once __DIR__.'/../../lib/dynamicsprices.lib.php';
                 //var_dump(getDolGlobalString('LMDB_SUPPLIER_BUYPRICE_ALTERED'));
