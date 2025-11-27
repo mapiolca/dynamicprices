1018c6c1b83b3a244704ba1fef6acfc4f19a91b1
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-11-27 14:56:40 +0100

    Add kit price recalculation triggers

--- core/triggers/interface_99_modDynamicsPrices_DynamicsPricesTriggers.class.php
+++ core/triggers/interface_99_modDynamicsPrices_DynamicsPricesTriggers.class.php
@@ -1,6 +1,5 @@
 <?php
-/* Copyright (C) 2023       Laurent Destailleur         <eldy@users.sourceforge.net>
- * Copyright (C) ---Replace with your own copyright and developer email---
+/* Copyright (C) 2025 Pierre Ardoin		<developpeur@lesmetiersdubatiment.fr>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,318 +16,146 @@
  */
 
 /**
- * \file    core/triggers/interface_99_modMyModule_MyModuleTriggers.class.php
- * \ingroup mymodule
- * \brief   Example of trigger file.
+ * \file    core/triggers/interface_99_modDynamicsPrices_DynamicsPricesTriggers.class.php
+ * \ingroup dynamicsprices
+ * \brief   Triggers for DynamicPrices module.
  *
- * You can create other triggered files by copying this one.
- * - File name should be either:
- *      - interface_99_modMyModule_MyTrigger.class.php
- *      - interface_99_all_MyTrigger.class.php
- * - The file must stay in core/triggers
- * - The class name must be InterfaceMyTrigger
+ * This class handles price recalculation hooks for DynamicPrices module.
  */
 
 require_once DOL_DOCUMENT_ROOT.'/core/triggers/dolibarrtriggers.class.php';
 
-
 /**
- *  Class of triggers for MyModule module
+ * Class of triggers for DynamicsPrices module
  */
 class InterfaceDynamicsPricesTriggers extends DolibarrTriggers
 {
-    /**
-     * Constructor
-     *
-     * @param DoliDB $db Database handler
-     */
-    public function __construct($db)
-    {
-        parent::__construct($db);
-        $this->family = "product";
-        $this->description = "Dynamicsprices triggers.";
-        $this->version = self::VERSIONS['dev'];
-        $this->picto = 'logo.png@dynamicsprices';
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param DoliDB $db Database handler
+	 */
+	public function __construct($db)
+	{
+		parent::__construct($db);
+		$this->family = 'product';
+		$this->description = 'Dynamicsprices triggers.';
+		$this->version = self::VERSIONS['dev'];
+		$this->picto = 'logo.png@dynamicsprices';
+	}
 
-    /**
-     * Function called when a Dolibarr business event is done.
-     * All functions "runTrigger" are triggered if the file is inside the directory core/triggers
-     *
-     * @param string        $action     Event action code
-     * @param CommonObject  $object     Object
-     * @param User          $user       Object user
-     * @param Translate     $langs      Object langs
-     * @param Conf          $conf       Object conf
-     * @return int                      Return integer <0 if KO, 0 if no triggered ran, >0 if OK
-     */
-    public function runTrigger($action, $object, User $user, Translate $langs, Conf $conf)
-    {
-        if (!isModEnabled('dynamicsprices')) {
-            return 0; // If module is not enabled, we do nothing
-        }
+	/**
+	 * Function called when a Dolibarr business event is done.
+	 * All functions "runTrigger" are triggered if the file is inside the directory core/triggers
+	 *
+	 * @param string       $action Event action code
+	 * @param CommonObject $object Object
+	 * @param User         $user   Object user
+	 * @param Translate    $langs  Object langs
+	 * @param Conf         $conf   Object conf
+	 * @return int                 Return integer <0 if KO, 0 if no triggered ran, >0 if OK
+	 */
+	public function runTrigger($action, $object, User $user, Translate $langs, Conf $conf)
+	{
+		global $db;
 
-        // Put here code you want to execute when a Dolibarr business events occurs.
-        // Data and type of action are stored into $object and $action
+		if (!isModEnabled('dynamicsprices')) {
+			return 0; // If module is not enabled, we do nothing
+		}
 
-        global $db;
+		if (!getDolGlobalString('LMDB_COST_PRICE_ONLY')) {
+			if (in_array($action, array('SUPPLIER_PRODUCT_BUYPRICE_CREATE', 'SUPPLIER_PRODUCT_BUYPRICE_MODIFY', 'SUPPLIER_PRODUCT_BUYPRICE_DELETE'), true)) {
+				require_once __DIR__.'/../../lib/dynamicsprices.lib.php';
+				if (getDolGlobalString('LMDB_SUPPLIER_BUYPRICE_ALTERED')) {
+					update_customer_prices_from_suppliers($db, $user, $langs, $conf, $this->getProductId($object));
+				}
+			}
+		} else {
+			if ($action === 'PRODUCT_MODIFY') {
+				require_once __DIR__.'/../../lib/dynamicsprices.lib.php';
+				if (getDolGlobalString('LMDB_SUPPLIER_BUYPRICE_ALTERED')) {
+					update_customer_prices_from_cost_price($db, $user, $langs, $conf, $this->getProductId($object));
+				}
+			}
+		}
 
-        if (!getDolGlobalString('LMDB_COST_PRICE_ONLY')) {
-            if ($action === 'SUPPLIER_PRODUCT_BUYPRICE_CREATE' || $action === 'SUPPLIER_PRODUCT_BUYPRICE_MODIFY' || $action == 'SUPPLIER_PRODUCT_BUYPRICE_DELETE') {
-                require_once __DIR__.'/../../lib/dynamicsprices.lib.php';
-                //var_dump(getDolGlobalString('LMDB_SUPPLIER_BUYPRICE_ALTERED'));
-                if (getDolGlobalString('LMDB_SUPPLIER_BUYPRICE_ALTERED')) {
-                    $results = update_customer_prices_from_suppliers($db, $user, $langs, $conf, $object->fk_product);
-                }
-                //var_dump($results);
-            }
-        } else {
-            if ($action === 'PRODUCT_MODIFY') {
-                require_once __DIR__.'/../../lib/dynamicsprices.lib.php';
-                //var_dump(getDolGlobalString('LMDB_COST_PRICE_ONLY'));
-                if (getDolGlobalString('LMDB_SUPPLIER_BUYPRICE_ALTERED')) {
-                    $results = update_customer_prices_from_cost_price($db, $user, $langs, $conf, $object->fk_product);
-                }
-            }
-        }
+		if (in_array($action, array('PRODUCT_MODIFY', 'PRODUCT_BUYPRICE_MODIFY', 'PRODUCT_BUYPRICE_DELETE'), true)) {
+			$this->triggerKitPriceUpdate($object, $user, $langs, $conf);
+		}
 
-        // You can isolate code for each action in a separate method: this method should be named like the trigger in camelCase.
-        // For example : COMPANY_CREATE => public function companyCreate($action, $object, User $user, Translate $langs, Conf $conf)
-        $methodName = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', strtolower($action)))));
-        $callback = array($this, $methodName);
-        if (is_callable($callback)) {
-            dol_syslog(
-                "Trigger '".$this->name."' for action '$action' launched by ".__FILE__.". id=".$object->id
-            );
+		$methodName = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', strtolower($action)))));
+		$callback = array($this, $methodName);
+		if (is_callable($callback)) {
+			dol_syslog(
+				"Trigger '".$this->name."' for action '$action' launched by ".__FILE__.". id=".$object->id
+			);
 
-            return call_user_func($callback, $action, $object, $user, $langs, $conf);
+			return call_user_func($callback, $action, $object, $user, $langs, $conf);
+		}
 
+		return 0;
+	}
 
-        }
+	/**
+	 * Launch kit price recalculation when a kit or its buy prices are updated.
+	 *
+	 * @param CommonObject $object Triggered object
+	 * @param User         $user   Current user
+	 * @param Translate    $langs  Language handler
+	 * @param Conf         $conf   Global configuration
+	 * @return int                 >0 if a kit was processed, 0 otherwise
+	 */
+	private function triggerKitPriceUpdate($object, User $user, Translate $langs, Conf $conf)
+	{
+		global $db;
 
-        // Or you can execute some code here
-        switch ($action) {  // @phan-suppress-current-line PhanNoopSwitchCases
-            // Users
-            //case 'USER_CREATE':
-            //case 'USER_MODIFY':
-            //case 'USER_NEW_PASSWORD':
-            //case 'USER_ENABLEDISABLE':
-            //case 'USER_DELETE':
+		$productId = $this->getProductId($object);
+		if (empty($productId)) {
+			return 0;
+		}
 
-            // Actions
-            //case 'ACTION_MODIFY':
-            //case 'ACTION_CREATE':
-            //case 'ACTION_DELETE':
+		dol_include_once('/product/class/product.class.php');
+		require_once __DIR__.'/../../lib/dynamicsprices.lib.php';
 
-            // Groups
-            //case 'USERGROUP_CREATE':
-            //case 'USERGROUP_MODIFY':
-            //case 'USERGROUP_DELETE':
+		$product = new Product($db);
+		if ($product->fetch($productId) <= 0) {
+			return 0;
+		}
 
-            // Companies
-            //case 'COMPANY_CREATE':
-            //case 'COMPANY_MODIFY':
-            //case 'COMPANY_DELETE':
+		$components = dynamicsPricesGetKitComponents($db, $product);
+		if (empty($components)) {
+			return 0;
+		}
 
-            // Contacts
-            //case 'CONTACT_CREATE':
-            //case 'CONTACT_MODIFY':
-            //case 'CONTACT_DELETE':
-            //case 'CONTACT_ENABLEDISABLE':
+		if (getDolGlobalString('LMDB_COST_PRICE_ONLY')) {
+			update_customer_prices_from_cost_price($db, $user, $langs, $conf, $productId);
+		} else {
+			update_customer_prices_from_suppliers($db, $user, $langs, $conf, $productId);
+		}
 
-            // Products
-            //case 'PRODUCT_CREATE':
-            //case 'PRODUCT_MODIFY':
-            //case 'PRODUCT_DELETE':
-            //case 'PRODUCT_PRICE_MODIFY':
-            //case 'PRODUCT_SET_MULTILANGS':
-            //case 'PRODUCT_DEL_MULTILANGS':
+		return 1;
+	}
 
-            //Stock movement
-            //case 'STOCK_MOVEMENT':
+	/**
+	 * Extract product id from triggered object.
+	 *
+	 * @param CommonObject $object Triggered object
+	 * @return int                 Product id if found, 0 otherwise
+	 */
+	private function getProductId($object)
+	{
+		if (!empty($object->fk_product)) {
+			return (int) $object->fk_product;
+		}
 
-            //MYECMDIR
-            //case 'MYECMDIR_CREATE':
-            //case 'MYECMDIR_MODIFY':
-            //case 'MYECMDIR_DELETE':
+		if (!empty($object->id)) {
+			return (int) $object->id;
+		}
 
-            // Sales orders
-            //case 'ORDER_CREATE':
-            //case 'ORDER_MODIFY':
-            //case 'ORDER_VALIDATE':
-            //case 'ORDER_DELETE':
-            //case 'ORDER_CANCEL':
-            //case 'ORDER_SENTBYMAIL':
-            //case 'ORDER_CLASSIFY_BILLED':     // TODO Replace it with ORDER_BILLED
-            //case 'ORDER_CLASSIFY_UNBILLED':   // TODO Replace it with ORDER_UNBILLED
-            //case 'ORDER_SETDRAFT':
-            //case 'LINEORDER_INSERT':
-            //case 'LINEORDER_MODIFY':
-            //case 'LINEORDER_DELETE':
+		if (!empty($object->productid)) {
+			return (int) $object->productid;
+		}
 
-            // Supplier orders
-            //case 'ORDER_SUPPLIER_CREATE':
-            //case 'ORDER_SUPPLIER_MODIFY':
-            //case 'ORDER_SUPPLIER_VALIDATE':
-            //case 'ORDER_SUPPLIER_DELETE':
-            //case 'ORDER_SUPPLIER_APPROVE':
-            //case 'ORDER_SUPPLIER_CLASSIFY_BILLED':        // TODO Replace with ORDER_SUPPLIER_BILLED
-            //case 'ORDER_SUPPLIER_CLASSIFY_UNBILLED':      // TODO Replace with ORDER_SUPPLIER_UNBILLED
-            //case 'ORDER_SUPPLIER_REFUSE':
-            //case 'ORDER_SUPPLIER_CANCEL':
-            //case 'ORDER_SUPPLIER_SENTBYMAIL':
-            //case 'ORDER_SUPPLIER_RECEIVE':
-            //case 'LINEORDER_SUPPLIER_DISPATCH':
-            //case 'LINEORDER_SUPPLIER_CREATE':
-            //case 'LINEORDER_SUPPLIER_MODIFY':
-            //case 'LINEORDER_SUPPLIER_DELETE':
-
-            // Proposals
-            //case 'PROPAL_CREATE':
-            //case 'PROPAL_MODIFY':
-            //case 'PROPAL_VALIDATE':
-            //case 'PROPAL_SENTBYMAIL':
-            //case 'PROPAL_CLASSIFY_BILLED':        // TODO Replace it with PROPAL_BILLED
-            //case 'PROPAL_CLASSIFY_UNBILLED':      // TODO Replace it with PROPAL_UNBILLED
-            //case 'PROPAL_CLOSE_SIGNED':
-            //case 'PROPAL_CLOSE_REFUSED':
-            //case 'PROPAL_DELETE':
-            //case 'LINEPROPAL_INSERT':
-            //case 'LINEPROPAL_MODIFY':
-            //case 'LINEPROPAL_DELETE':
-
-            // SupplierProposal
-            //case 'SUPPLIER_PROPOSAL_CREATE':
-            //case 'SUPPLIER_PROPOSAL_MODIFY':
-            //case 'SUPPLIER_PROPOSAL_VALIDATE':
-            //case 'SUPPLIER_PROPOSAL_SENTBYMAIL':
-            //case 'SUPPLIER_PROPOSAL_CLOSE_SIGNED':
-            //case 'SUPPLIER_PROPOSAL_CLOSE_REFUSED':
-            //case 'SUPPLIER_PROPOSAL_DELETE':
-            //case 'LINESUPPLIER_PROPOSAL_INSERT':
-            //case 'LINESUPPLIER_PROPOSAL_MODIFY':
-            //case 'LINESUPPLIER_PROPOSAL_DELETE':
-
-            // Contracts
-            //case 'CONTRACT_CREATE':
-            //case 'CONTRACT_MODIFY':
-            //case 'CONTRACT_ACTIVATE':
-            //case 'CONTRACT_CANCEL':
-            //case 'CONTRACT_CLOSE':
-            //case 'CONTRACT_DELETE':
-            //case 'LINECONTRACT_INSERT':
-            //case 'LINECONTRACT_MODIFY':
-            //case 'LINECONTRACT_DELETE':
-
-            // Bills
-            //case 'BILL_CREATE':
-            //case 'BILL_MODIFY':
-            //case 'BILL_VALIDATE':
-            //case 'BILL_UNVALIDATE':
-            //case 'BILL_SENTBYMAIL':
-            //case 'BILL_CANCEL':
-            //case 'BILL_DELETE':
-            //case 'BILL_PAYED':
-            //case 'LINEBILL_INSERT':
-            //case 'LINEBILL_MODIFY':
-            //case 'LINEBILL_DELETE':
-
-            // Recurring Bills
-            //case 'BILLREC_MODIFY':
-            //case 'BILLREC_DELETE':
-            //case 'BILLREC_AUTOCREATEBILL':
-            //case 'LINEBILLREC_MODIFY':
-            //case 'LINEBILLREC_DELETE':
-
-            //Supplier Bill
-            //case 'BILL_SUPPLIER_CREATE':
-            //case 'BILL_SUPPLIER_MODIFY':
-            //case 'BILL_SUPPLIER_DELETE':
-            //case 'BILL_SUPPLIER_PAYED':
-            //case 'BILL_SUPPLIER_UNPAYED':
-            //case 'BILL_SUPPLIER_VALIDATE':
-            //case 'BILL_SUPPLIER_UNVALIDATE':
-            //case 'LINEBILL_SUPPLIER_CREATE':
-            //case 'LINEBILL_SUPPLIER_MODIFY':
-            //case 'LINEBILL_SUPPLIER_DELETE':
-
-            // Payments
-            //case 'PAYMENT_CUSTOMER_CREATE':
-            //case 'PAYMENT_SUPPLIER_CREATE':
-            //case 'PAYMENT_ADD_TO_BANK':
-            //case 'PAYMENT_DELETE':
-
-            // Online
-            //case 'PAYMENT_PAYBOX_OK':
-            //case 'PAYMENT_PAYPAL_OK':
-            //case 'PAYMENT_STRIPE_OK':
-
-            // Donation
-            //case 'DON_CREATE':
-            //case 'DON_MODIFY':
-            //case 'DON_DELETE':
-
-            // Interventions
-            //case 'FICHINTER_CREATE':
-            //case 'FICHINTER_MODIFY':
-            //case 'FICHINTER_VALIDATE':
-            //case 'FICHINTER_CLASSIFY_BILLED':         // TODO Replace it with FICHINTER_BILLED
-            //case 'FICHINTER_CLASSIFY_UNBILLED':       // TODO Replace it with FICHINTER_UNBILLED
-            //case 'FICHINTER_DELETE':
-            //case 'LINEFICHINTER_CREATE':
-            //case 'LINEFICHINTER_MODIFY':
-            //case 'LINEFICHINTER_DELETE':
-
-            // Members
-            //case 'MEMBER_CREATE':
-            //case 'MEMBER_VALIDATE':
-            //case 'MEMBER_SUBSCRIPTION':
-            //case 'MEMBER_MODIFY':
-            //case 'MEMBER_NEW_PASSWORD':
-            //case 'MEMBER_RESILIATE':
-            //case 'MEMBER_DELETE':
-
-            // Categories
-            //case 'CATEGORY_CREATE':
-            //case 'CATEGORY_MODIFY':
-            //case 'CATEGORY_DELETE':
-            //case 'CATEGORY_SET_MULTILANGS':
-
-            // Projects
-            //case 'PROJECT_CREATE':
-            //case 'PROJECT_MODIFY':
-            //case 'PROJECT_DELETE':
-
-            // Project tasks
-            //case 'TASK_CREATE':
-            //case 'TASK_MODIFY':
-            //case 'TASK_DELETE':
-
-            // Task time spent
-            //case 'TASK_TIMESPENT_CREATE':
-            //case 'TASK_TIMESPENT_MODIFY':
-            //case 'TASK_TIMESPENT_DELETE':
-            //case 'PROJECT_ADD_CONTACT':
-            //case 'PROJECT_DELETE_CONTACT':
-            //case 'PROJECT_DELETE_RESOURCE':
-
-            // Shipping
-            //case 'SHIPPING_CREATE':
-            //case 'SHIPPING_MODIFY':
-            //case 'SHIPPING_VALIDATE':
-            //case 'SHIPPING_SENTBYMAIL':
-            //case 'SHIPPING_BILLED':
-            //case 'SHIPPING_CLOSED':
-            //case 'SHIPPING_REOPEN':
-            //case 'SHIPPING_DELETE':
-
-            // and more...
-
-            default:
-                dol_syslog("Trigger '".$this->name."' for action '".$action."' launched by ".__FILE__.". id=".$object->id);
-                break;
-        }
-
-        return 0;
-    }
+		return 0;
+	}
 }
